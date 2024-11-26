cdf1de9b10ceca1c056cea859b7aa662af6d859c
Giovanni-Serra-Git <giovanni.serra.web@gmail.com>
2024-11-26 10:50:51 +0100

    Create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from 'dotenv'
+import useRoutes from "./routes/user.route.js"
 
 dotenv.config()
 
@@ -13,3 +14,5 @@ const app = express();
 app.listen(3000, () => {
     console.log("The server is running ")
 })
+
+app.use("/api/user", useRoutes)
